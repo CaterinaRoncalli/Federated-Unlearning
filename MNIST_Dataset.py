648d4cf5243f315ba676ed3b401d28f2d7e5a307
Caterina Roncalli <caterina.roncalli@studium.uni-hamburg.de>
2022-12-16 09:16:24 +0100

    dataset with correct reshaping

--- MNIST_Dataset.py
+++ MNIST_Dataset.py
@@ -1,19 +1,23 @@
+import numpy as np
+import torch
+from mnist import MNIST
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision import transforms
-import numpy as np
-from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-from mnist import MNIST
+from torchvision.transforms import InterpolationMode
 
 
 class MNISTDataSet(Dataset):
-    def __init__(self, images: np.ndarray, labels: np.ndarray, transform=None):
+    def __init__(self, images: np.ndarray, labels: np.ndarray, transform: bool = True):
         self.images = images
         self.labels = labels
         if transform:
             self.transforms = transforms.Compose([transforms.ToTensor(),
-                                                  transforms.Resize((224, 224)),
-                                                  transforms.Normalize((0.1307,), (0.3081,))])
+                                                  transforms.Normalize(0.1307, 0.3081),
+                                                  transforms.Resize((224, 224),
+                                                                    interpolation=InterpolationMode.NEAREST),
+                                                  ])
         else:
             self.transforms = transforms.Compose([transforms.ToTensor()])
 
@@ -21,8 +25,10 @@ class MNISTDataSet(Dataset):
         return len(self.images)
 
     def __getitem__(self, item):
-        images = self.transforms(self.images)
-        return images[item], self.labels[item]
+        image = self.images[item]
+        label = self.labels[item]
+        image = self.transforms(image).to(torch.half)
+        return image, label
 
 
 if __name__ == "__main__":
@@ -31,8 +37,11 @@ if __name__ == "__main__":
         indices = np.arange(len(images))
         np.random.shuffle(indices)
         splits = np.split(indices, n_clients)
-        images = images[splits]
-        labels = labels[splits]
+        client_images = []
+        client_labels = []
+        for split in splits:
+            client_images.append(images[split])
+            client_labels.append(labels[split])
         return images, labels
 
 

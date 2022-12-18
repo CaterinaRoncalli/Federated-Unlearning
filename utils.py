0d9d08d965bb74a90489a9911994388f3952aa97
Caterina Roncalli <caterina.roncalli@studium.uni-hamburg.de>
2022-12-18 18:05:59 +0100

    test for final model evaluation

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 from MNIST_Dataset import MNISTDataSet
 import numpy as np
+from sklearn import metrics
+import torch.nn as nn
+from torch import inference_mode
+from torch.cuda.amp import autocast
 from torch.utils.data import DataLoader
+from typing import List
 
 
-def client_split(images: np.ndarray, labels: np.ndarray, n_clients: int):
+def client_split(images: np.ndarray, labels: np.ndarray, n_clients: int) -> (List[np.ndarray], List[np.ndarray]):
     indices = np.arange(len(images))
     np.random.shuffle(indices)
     splits = np.split(indices, n_clients)
@@ -20,7 +25,7 @@ def build_client_loaders(client_images: np.ndarray,
                          batch_size: int,
                          num_workers: int,
                          shuffle: bool,
-                         persistent_workers: bool):
+                         persistent_workers: bool) -> List[DataLoader]:
     client_loaders = []
     for images, labels in zip(client_images, client_labels):
         client_set = MNISTDataSet(images, labels)
@@ -28,3 +33,24 @@ def build_client_loaders(client_images: np.ndarray,
                                    shuffle=shuffle, num_workers=num_workers, persistent_workers=persistent_workers)
         client_loaders.append(client_loader)
     return client_loaders
+
+
+@autocast()
+@inference_mode()
+def test_model(test_loader: DataLoader,
+               model: nn.Module,
+               device: str | int = 'cuda',
+               metric: callable = metrics.balanced_accuracy_score) -> float:
+    output_list = []
+    label_list = []
+    model.eval()
+    model.to(device)
+    for idx, (inputs, labels) in enumerate(test_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        output_list.extend(outputs.detach().tolist())
+        label_list.extend(labels.detach().tolist())
+    pred = np.argmax(np.array(output_list), axis=1)
+    metric = metric(np.array(label_list), pred)
+    print(f'model test metric value: {metric}')
+    return metric

29dd854ec52d3f2596f9e90cd87ca7cc05fe82e2
Caterina Roncalli <caterina.roncalli@studium.uni-hamburg.de>
2022-12-18 18:08:37 +0100

    centralized and federated train with custom CNN and final testing

--- mnist_centralized_train.py
+++ mnist_centralized_train.py
@@ -1,28 +1,57 @@
-import sklearn.metrics
-import timm
-import torch
+from mnist import MNIST
+from MNIST_Dataset import MNISTDataSet
+import numpy as np
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 import torch.nn as nn
-import torchvision
 from torch import optim
+from torch.utils.data import DataLoader
+import wandb
 
+from cnn_model import CNN
 from gym import Gym
+from utils import test_model
 
-train_set = torchvision.datasets.MNIST('files', train=True, download=True, transform=torchvision.transforms.Compose(
-    [torchvision.transforms.Resize((224, 224)),
-     torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))]))
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=6, persistent_workers=True)
 
-test_set = torchvision.datasets.MNIST('files', train=False, download=True, transform=torchvision.transforms.Compose(
-    [torchvision.transforms.Resize((224, 224)),
-     torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))]))
-test_loader = torch.utils.data.DataLoader(train_set, batch_size=1024, shuffle=True, num_workers=6, persistent_workers=True)
+wandb.init(project="centralized learning", entity="federated_unlearning", group='MNIST')
 
-model = timm.models.xcit_nano_12_p16_224_dist(pretrained=True, in_chans=1, num_classes=10)
+mnist_data = MNIST('files/MNIST/raw/samples')
+images, labels = mnist_data.load_training()
+test_images, test_labels = mnist_data.load_testing()
+
+# reshape images and convert to floats
+images = np.array(images).reshape(-1, 28, 28, 1) / 255
+test_images = np.array(test_images).reshape(-1, 28, 28, 1) / 255
+
+#split train images into train/val split
+split = train_test_split(images, labels, stratify=labels, train_size=0.7)
+train_images, val_images, train_labels, val_labels = split
+
+train_labels = np.array(train_labels)
+val_labels = np.array(val_labels)
+test_labels = np.array(test_labels)
+
+# build Dataloaders
+train_set = MNISTDataSet(train_images, train_labels)
+train_loader = DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2, persistent_workers=True)
+val_set = MNISTDataSet(val_images, val_labels)
+val_loader = DataLoader(val_set, batch_size=128, shuffle=True, num_workers=8, persistent_workers=True)
+test_set = MNISTDataSet(test_images, test_labels)
+test_loader = DataLoader(test_set, batch_size=128, shuffle=True, num_workers=8, persistent_workers=True)
+
+device = "cuda"
+model = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
+epochs = 10
 
-gym = Gym(train_loader=train_loader, val_loader=test_loader, model=model, criterion=criterion, optimizer=optimizer,
-          device="cuda", verbose=True, metric=sklearn.metrics.balanced_accuracy_score)
-gym.train(100, 100)
+wandb.config = {
+  "epochs": epochs,
+  "batch_size": 128
+}
 
-print("hic featurecloud est")
+gym = Gym(train_loader=train_loader, val_loader=val_loader, model=model, criterion=criterion,
+          optimizer=optimizer, device=device, verbose=True, metric=metrics.balanced_accuracy_score,
+          name="central model", log=True)
+central_model = gym.train(epochs=epochs, eval_interval=100)
+metric = test_model(test_loader=test_loader, model=central_model, device=device, metric=metrics.balanced_accuracy_score)
--- mnist_federated_train.py
+++ mnist_federated_train.py
@@ -1,38 +1,18 @@
-from mnist import MNIST
-from MNIST_Dataset import MNISTDataSet
 import numpy as np
-import timm
 import torch.nn as nn
+from mnist import MNIST
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from torch import optim
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
+import wandb
+from MNIST_Dataset import MNISTDataSet
+from cnn_model import CNN
 from gym import FederatedGym
+from utils import client_split, build_client_loaders, test_model
 
 
-def client_split(images: np.ndarray, labels: np.ndarray, n_clients: int):
-    indices = np.arange(len(images))
-    np.random.shuffle(indices)
-    splits = np.split(indices, n_clients)
-    client_images = []
-    client_labels = []
-    for split in splits:
-        client_images.append(images[split])
-        client_labels.append(labels[split])
-    return client_images, client_labels
-
-def build_client_loaders(client_images: np.ndarray,
-                         client_labels: np.ndarray,
-                         batch_size: int,
-                         num_workers: int,
-                         shuffle: bool,
-                         persistent_workers: bool):
-    client_loaders = []
-    for images, labels in zip(client_images, client_labels):
-        client_set = MNISTDataSet(images, labels)
-        client_loader = DataLoader(client_set, batch_size=batch_size,
-                                   shuffle=shuffle, num_workers=num_workers, persistent_workers=persistent_workers)
-        client_loaders.append(client_loader)
-    return client_loaders
+wandb.init(project="federated learning", entity="federated_unlearning", group='MNIST')
 
 n_clients = 5
 mnist_data = MNIST('files/MNIST/raw/samples')
@@ -41,30 +21,47 @@ test_images, test_labels = mnist_data.load_testing()
 
 # reshape images and convert to floats
 images = np.array(images).reshape(-1, 28, 28, 1) / 255
-#split train images into train/val split
+test_images = np.array(test_images).reshape(-1, 28, 28, 1) / 255
+
+# split train images into train/val split
 split = train_test_split(images, labels, stratify=labels, train_size=0.7)
 train_images, val_images, train_labels, val_labels = split
+
 train_labels = np.array(train_labels)
+val_labels = np.array(val_labels)
 test_labels = np.array(test_labels)
 
-#split MNIST train data into n clients
+# split MNIST train data into n clients
 client_train_images, client_train_labels = client_split(train_images, train_labels, n_clients)
 
-#build list with client loaders for local training
+# build list with client loaders for local training
 client_train_loaders = build_client_loaders(client_images=client_train_images, client_labels=client_train_labels,
-                                            batch_size=128, shuffle=True, num_workers=6, persistent_workers=True)
+                                            batch_size=128, shuffle=True, num_workers=2, persistent_workers=False)
 
-#build val loader for global model evaluation
+# build val loader for global model evaluation
 val_set = MNISTDataSet(val_images, val_labels)
-val_loader = DataLoader(val_set, batch_size=128, shuffle=True, num_workers=6, persistent_workers=True)
+val_loader = DataLoader(val_set, batch_size=128, shuffle=True, num_workers=2, persistent_workers=False)
 
+# build test loader for final global model evaluation
+test_set = MNISTDataSet(test_images, test_labels)
+test_loader = DataLoader(test_set, batch_size=128, shuffle=True, num_workers=2, persistent_workers=False)
 
-model = timm.models.efficientnet_b0(pretrained=True, in_chans=1, num_classes=10)
+device = "cuda"
+model = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam
-rounds = 10
+rounds = 2
 epochs = 1
 
-fed_gym = FederatedGym(client_train_loaders=client_train_loaders, val_loader=val_loader, model=model,
-                       optimizer=optimizer, criterion=criterion, rounds=rounds, epochs=epochs)
+wandb.config = {
+    "client_number": n_clients,
+    "rounds": rounds,
+    "batch_size": 128
+}
+
+fed_gym = FederatedGym(client_train_loaders=client_train_loaders, val_loader=val_loader,
+                       model=model, optimizer=optimizer, criterion=criterion, device=device,
+                       metric=metrics.balanced_accuracy_score,
+                       rounds=rounds, epochs=epochs, log=False)
 global_model = fed_gym.train()
+metric = test_model(test_loader=test_loader, model=global_model, device=device, metric=metrics.balanced_accuracy_score)

e162a233177e010b8a95dd044a4b17dfd926b8db
Caterina Roncalli <caterina.roncalli@studium.uni-hamburg.de>
2022-12-29 17:06:27 +0100

    Dataset with optional backdoor data

--- MNIST_Dataset.py
+++ MNIST_Dataset.py
@@ -2,15 +2,20 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 import numpy as np
-import cv2
-#import via opencv-python
 
-# nur für eine klasse poisonen --> nur für einen Client
+
 class MNISTDataSet(Dataset):
-    def __init__(self, images: np.ndarray, labels: np.ndarray, backdoor: np.ndarray, transform=True):
+    def __init__(self, images: np.ndarray,
+                 labels: np.ndarray,
+                 backdoor_old_label: int | None = None,
+                 backdoor_new_label: int | None = None,
+                 transform: bool = True,
+                 backdoor: bool = False
+                 ):
         self.images = images
         self.labels = labels
-        self.backdoor = backdoor
+        if backdoor:
+            self._backdoor(backdoor_old_label, backdoor_new_label)
         if transform:
             self.transforms = transforms.Compose([transforms.ToTensor(),
                                                   transforms.Normalize(0.1307, 0.3081)])
@@ -23,23 +28,12 @@ class MNISTDataSet(Dataset):
     def __getitem__(self, item):
         image = self.images[item]
         label = self.labels[item]
-        image = self.transforms(image).to(torch.half)
-        return image, label
+        image = self.transforms(image)
+        return image.to(torch.half), label
 
-    def __backdoor__(self, change):
-        images = self.images
-        labels = self.labels
-        backdoorimages = np.empty
-        backdoorlabels = np.empty
-        for image, label in zip(images, labels):
-            if label == change:
-                backdoorimage = image.deepcopy()
-                backdoorimage = cv2.rectangle(backdoorimage, (24, 24), (25, 25), 250, -5)
-                backdoorimage = cv2.rectangle(backdoorimage, (1, 1), (2, 2), 250, -5)
-                backdoorlabel = (label + 1) % 10
-                np.append(backdoorimages, backdoorimage)
-                np.append(backdoorlabels, backdoorlabel)
-        images = np.concatenate(images, backdoorimages)
-        labels = np.concatenate(labels, backdoorlabels)
-        return images, labels
+    def _backdoor(self, old_label: int, new_label: int):
+        backdoored_images = self.images[self.labels == old_label]
+        backdoored_images[:, 26:, 26:] = 1
+        self.images[self.labels == old_label] = backdoored_images
+        self.labels[self.labels == old_label] = new_label
 

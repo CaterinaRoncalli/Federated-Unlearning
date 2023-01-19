4e4937ddeae422f16912ee1bc6924c2a3709c142
Caterina Roncalli <caterina.roncalli@studium.uni-hamburg.de>
2023-01-19 17:44:53 +0100

    updated pipeline for fed training with different client distributions

--- mnist_federated_train.py
+++ mnist_federated_train.py
@@ -1,12 +1,13 @@
 import os
-
+import random
+import logging
 import numpy as np
 import petname
 import torch
 import torch.nn as nn
 from mnist import MNIST
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from torch import optim
 from torch.utils.data import DataLoader
 
@@ -14,33 +15,34 @@ import wandb
 from MNIST_Dataset import MNISTDataSet
 from cnn_model import CNN
 from gym import FederatedGym, FederatedUnlearnGym
-from utils import client_split, build_client_loaders, test_model
+from utils import client_split, build_client_loaders, test_model, calc_model_dist
 
-model_path = "saved_models/federated"
+
 n_clients = 3
-mnist_data = MNIST('files/MNIST/raw')
-images, labels = mnist_data.load_training()
-test_images, test_labels = mnist_data.load_testing()
+client_train_folder = f"client_images/homogeneous_dist/n_clients_{n_clients}"
+client_val_folder = f"client_images/val"
+client_test_folder = f"client_images/test"
+model_path = "saved_models/federated"
 
-'''reshape images and convert to floats'''
-images = np.array(images).reshape(-1, 28, 28, 1) / 255
-test_images = np.array(test_images).reshape(-1, 28, 28, 1) / 255
+val_images = np.load(client_val_folder+"/val.npz")["images"]
+val_labels = np.load(client_val_folder+"/val.npz")["labels"]
 
-'''split train images into train/val split'''
-split = train_test_split(images, labels, stratify=labels, train_size=0.7)
-train_images, val_images, train_labels, val_labels = split
-train_labels = np.array(train_labels)
-val_labels = np.array(val_labels)
-test_labels = np.array(test_labels)
+test_images = np.load(client_test_folder+"/test.npz")["images"]
+test_labels = np.load(client_test_folder+"/test.npz")["labels"]
 
-'''split MNIST train data into n clients'''
-client_train_images, client_train_labels = client_split(train_images, train_labels, n_clients)
+client_train_images = []
+client_train_labels = []
+
+for idx in range(n_clients):
+    client_train_images.append(np.load(client_train_folder+f"/client_{idx}.npz")["images"])
+    client_train_labels.append(np.load(client_train_folder+f"/client_{idx}.npz")["labels"])
+
 
 '''build list with client loaders for local training'''
 backdoor_clients = [True, False, False]
 backdoor_old_labels = list(range(10))
-backdoor_new_label = 10
-backdoor_prob = 0.2
+backdoor_new_label = 9
+backdoor_prob = 0.3
 client_train_loaders = build_client_loaders(client_images=client_train_images, client_labels=client_train_labels,
                                             backdoor_prob=backdoor_prob,
                                             backdoor_clients=backdoor_clients, backdoor_old_labels=backdoor_old_labels,
@@ -63,6 +65,8 @@ backdoor_test_loader = DataLoader(backdoor_test_set, batch_size=128, shuffle=Tru
                                   persistent_workers=False)
 
 
+
+
 '''initialization for training'''
 device = "cuda"
 model = CNN()
@@ -71,16 +75,20 @@ optimizer = optim.AdamW
 optimizer_params = {'lr': 0.001, 'weight_decay': 0.05}
 rounds = 20
 epochs = 1
-log = False
-model_saving = False
+log = True
+model_saving = True
 
 if log:
     wandb.init(project="federated learning", entity="federated_unlearning", group='MNIST')
     wandb.config = {
-        "client_number": n_clients,
         "rounds": rounds,
-        "batch_size": 128
+        "batch_size": 128,
+        "lr": 0.001,
+        "weight decay": 0.05,
+        "n clients": n_clients,
+        "removed client": 0
     }
+    logging.basicConfig(filename='logging/federated_backdoor.txt', encoding='utf-8', level=logging.INFO)
 
 '''federated training'''
 
@@ -99,13 +107,17 @@ global_metric = test_model(test_loader=test_loader, model=global_model, device=d
 
 '''global model testing on backdoored data'''
 print("Testing global model on backdoor test set")
-backdoor_new_label_metric = test_model(test_loader=backdoor_test_loader, model=global_model, device=device,
+backdoor_metric = test_model(test_loader=backdoor_test_loader, model=global_model, device=device,
                                        metric=metrics.balanced_accuracy_score)
 
 '''optional model saving'''
 if model_saving:
     if log:
         model_name = wandb.run.name + f"_acc_{global_metric:.4f}"
+        logging.info(f"federated training with {n_clients} homogeneous clients, removed client 0\n"
+                     f"model name: {model_name}\n"
+                     f"global model test accuracy: {global_metric}\n"
+                     f"global model backdoor accuracy: {backdoor_metric}")
     else:
         model_name = petname.generate(3, "_") + f"_acc_{global_metric:.4f}"
     path = os.path.join(model_path, model_name)
@@ -113,6 +125,7 @@ if model_saving:
     torch.save(global_model.state_dict(), os.path.join(path, model_name + "_global"))
     for idx, client_model in enumerate(client_models):
         torch.save(client_model.state_dict(), os.path.join(path, model_name + f"_client_{idx + 1}"))
+
 
 
 '''
@@ -145,6 +158,7 @@ delta = None
 tau = 0.12
 optimizer = optim.AdamW
 optimizer_params = {'lr': 0.001, 'weight_decay': 0.05}
+#untrain_optimizer = optim.SGD(unclient_model.parameters(), lr=0.01, momentum=0.9)
 untrain_optimizer = optim.AdamW(unclient_model.parameters(), lr=0.001, weight_decay=0.05)
 
 print('-----starting federated unlearning------')
@@ -159,18 +173,80 @@ unfed_gym = FederatedUnlearnGym(unclient_model=unclient_model,
                                 metric=metrics.balanced_accuracy_score,
                                 delta=delta, tau=tau, log=log)
 
-untrained_global_model, untrained_client_models, untrained_client_model = unfed_gym.untrain(client_untrain_epochs=5,
-                                                                                            federated_epochs=1,
-                                                                                            federated_rounds=1,
-                                                                                            untrain_optimizer=untrain_optimizer)
+untrained_global_model, untrained_client_models, removed_client_model = unfed_gym.untrain(client_untrain_epochs=5,
+                                                                                          federated_epochs=1,
+                                                                                          federated_rounds=1,
+                                                                                          untrain_optimizer=untrain_optimizer)
 
 '''global model testing on official MNIST test set'''
 print("Testing global unlearned model on MNIST test set")
-global_metric = test_model(test_loader=test_loader, model=untrained_global_model, device=device,
+un_global_metric = test_model(test_loader=test_loader, model=untrained_global_model, device=device,
                            metric=metrics.accuracy_score)
 
 '''global model testing on backdoored data'''
 print("Testing global unlearned model on backdoor test set")
-backdoor_new_label_metric = test_model(test_loader=backdoor_test_loader, model=untrained_global_model, device=device,
+un_backdoor_metric = test_model(test_loader=backdoor_test_loader, model=untrained_global_model, device=device,
                                        metric=metrics.balanced_accuracy_score)
+
+'''local model testing on official MNIST test set'''
+print("Testing local unlearned model on MNIST test set")
+un_local_metric = test_model(test_loader=test_loader, model=removed_client_model, device=device,
+                             metric=metrics.accuracy_score)
+
+'''global model testing on backdoored data'''
+print("Testing global unlearned model on backdoor test set")
+un_backdoor_local_metric = test_model(test_loader=backdoor_test_loader, model=removed_client_model, device=device,
+                                      metric=metrics.balanced_accuracy_score)
+
+print("Calculating global model - unlearned global model distance")
+gl_un_gl_model_dist, gl_un_gl_mean_dist = calc_model_dist(global_model, untrained_global_model)
+print("Global model - unlearned global model distance:")
+print(gl_un_gl_model_dist)
+print(gl_un_gl_mean_dist)
+print("Calculating local model - global model distance")
+print("local model - global model distance:")
+lo_gl_model_dist, lo_gl_mean_dist = calc_model_dist(removed_client_model, global_model)
+print(lo_gl_model_dist)
+print(lo_gl_mean_dist)
+print("Calculating local model - unlearned global model distance")
+print("local model - unlearned global model distance:")
+lo_un_gl_model_dist, lo_un_gl_mean_dist = calc_model_dist(removed_client_model, untrained_global_model)
+print(lo_un_gl_model_dist)
+print(lo_un_gl_mean_dist)
+
+
+if model_saving:
+    if log:
+        model_name = wandb.run.name + f"_acc_{global_metric:.4f}_unlearned"
+        logging.info(f"after removing client 0\n"
+                     f"model name: {model_name}\n"
+                     f"unlearned global model test accuracy: {un_global_metric}\n"
+                     f"unlearned global model backdoor accuracy: {un_backdoor_metric}\n"
+                     f"unlearned local model test accuracy: {un_local_metric}\n"
+                     f"unlearned local model backdoor accuracy: {un_backdoor_local_metric}\n"
+                     f"global model - unlearned global model distances: {gl_un_gl_model_dist}\n"
+                     f"global model - unlearned global model mean distance: {gl_un_gl_mean_dist}\n"
+                     f"local model - global model distances: {lo_gl_model_dist}\n"
+                     f"local model - global model mean distance: {lo_gl_mean_dist}\n"
+                     f"local model - unlearned global model distances: {lo_un_gl_model_dist}\n"
+                     f"local model - unlearned global mean distance: {lo_un_gl_mean_dist}\n"
+                     )
+    else:
+        model_name = petname.generate(3, "_") + f"_acc_{un_global_metric:.4f}_unlearned"
+    path = os.path.join(model_path, model_name)
+    os.mkdir(path)
+    torch.save(untrained_global_model.state_dict(), os.path.join(path, model_name + "_global"))
+    torch.save(removed_client_model.state_dict(), os.path.join(path, model_name + "_removed_client"))
+    for idx, client_model in enumerate(untrained_client_models):
+        torch.save(client_model.state_dict(), os.path.join(path, model_name + f"_client_{idx + 1}"))
+
+del global_model
+del untrained_global_model
+del removed_client_model
+del client_models
+del untrained_client_models
+
+
+
+
 
